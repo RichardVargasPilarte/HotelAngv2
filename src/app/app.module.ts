21da59a0cd589fdaeed289763039d8df525dc7f7
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-06-28 10:05:00 -0600

    Add SharedModule, AuthModule, ComponentsModule, HttpClientModule, jwtInterceptor, UserGuard, and MAT_DATE_LOCALE provider to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+// Modulos
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
+import { ComponentsModule } from './components/components.module';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { jwtInterceptor } from './interceptors/jwt.interceptor';
+import {UserGuard } from '../app/guards/user.guard';
+
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { DatePipe } from '@angular/common';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -13,9 +26,21 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    AuthModule,
+    ComponentsModule,
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    {provide: MAT_DATE_LOCALE, useValue: 'es-ES'},
+    DatePipe,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: jwtInterceptor,
+      multi: true,
+    },
+    UserGuard
   ],
   bootstrap: [AppComponent]
 })
