90350427331038ae8f13a85247f52eb85aed16d9
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-06-27 19:25:55 -0600

    Add environment configuration files and update angular.json for environment-specific settings

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  API_Domain: 'https://hotel-api-v2.onrender.com',
+  API_Port: 80,
+  API_EndPoint: 'api',
+  API_URI: 'https://hotel-api-v2.onrender.com/api',
+  API_Auth: 'https://hotel-api-v2.onrender.com/api/token/',
+  Api_Auth_Verify: 'https://hotel-api-v2.onrender.com/api/token/verify/',
+  APP_name: 'SPA',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  API_Domain: 'localhost:8000',
+  API_Port: 8000,
+  API_EndPoint: 'api',
+  API_URI: 'http://localhost:8000/api',
+  API_Auth: 'http://localhost:8000/api/token/',
+  Api_Auth_Verify: 'http://localhost:8000/api/token/verify/',
+  APP_name: 'SPA',
+};
