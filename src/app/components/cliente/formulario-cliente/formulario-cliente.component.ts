e8ab6d02004f64ce7288bfd0709fd57713cef55b
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-06-28 08:53:47 -0600

    Add client form and list components with validation and CRUD operations

--- src/app/components/cliente/formulario-cliente/formulario-cliente.component.ts
+++ src/app/components/cliente/formulario-cliente/formulario-cliente.component.ts
@@ -1,10 +1,125 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { ClienteService } from '../../../service/cliente.service';
+import { Cliente, IClientResponse } from '../../../models/cliente.model';
+
+interface DialogData {
+  type: string;
+  client?: Cliente | IClientResponse;
+}
 
 @Component({
   selector: 'app-formulario-cliente',
   templateUrl: './formulario-cliente.component.html',
-  styleUrl: './formulario-cliente.component.scss'
+  styleUrl: './formulario-cliente.component.scss',
 })
 export class FormularioClienteComponent {
+  public clientes = new Cliente();
+  public edit!: boolean;
+  subs: Subscription[] = [];
+  public selected = '0';
+  public form!: FormGroup;
 
+  TipoDocumento: string[] = ['Cedula', 'Pasaporte'];
+
+  constructor(
+    private clienteServicio: ClienteService,
+    public dialogRef: MatDialogRef<FormularioClienteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private fb: FormBuilder
+  ) {
+    this.createForm();
+  }
+
+  createForm(id?: string): void {
+    if (this.data.type === 'c') {
+      this.form = this.fb.group({
+        id: new FormControl(-1),
+        nombre: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+        ]),
+        apellido: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+        ]),
+        telefono: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+        ]),
+        tipo_identificacion: new FormControl('', [Validators.required]),
+        num_identificacion: new FormControl('', [
+          Validators.required,
+          Validators.minLength(16),
+        ]),
+        email: new FormControl('', [Validators.email]),
+        eliminado: new FormControl('NO'),
+      });
+    } else {
+      const cliente: IClientResponse = this.data.client as IClientResponse;
+      this.form = this.fb.group({
+        id: cliente.id,
+        nombre: new FormControl(cliente.nombre, [
+          Validators.required,
+          Validators.minLength(3),
+        ]),
+        apellido: new FormControl(cliente.apellido, [
+          Validators.required,
+          Validators.minLength(5),
+        ]),
+        telefono: new FormControl(cliente.telefono, [
+          Validators.required,
+          Validators.minLength(9),
+        ]),
+        tipo_identificacion: new FormControl(cliente.tipo_identificacion, [
+          Validators.required,
+          Validators.minLength(9),
+        ]),
+        num_identificacion: new FormControl(cliente.num_identificacion, [
+          Validators.required,
+          Validators.minLength(16),
+        ]),
+        email: new FormControl(cliente.email, [Validators.email]),
+        eliminado: new FormControl('NO'),
+      });
+    }
+  }
+
+  saveCustomer(): void {
+    let client = new Cliente();
+    client = Object.assign(client, this.form.value);
+    this.subs.push(
+      this.clienteServicio.addCustomer(client).subscribe({
+        next: () => {
+          this.dialogRef.close();
+        },
+        error: (error: any) => console.log('Hubo un error' + error),
+      })
+    );
+  }
+
+  updateClient(): void {
+    let client = new Cliente();
+    client = Object.assign(client, this.form.value);
+    this.subs.push(
+      this.clienteServicio.updateClient(client.id!, client).subscribe({
+        next: () => {
+          this.dialogRef.close();
+          (error: any) => console.log('Hubo un error' + error);
+        },
+      })
+    );
+  }
+
+  get Form(): any {
+    return this.form.controls;
+  }
 }
--- src/app/components/cliente/listado-cliente/listado-cliente.component.ts
+++ src/app/components/cliente/listado-cliente/listado-cliente.component.ts
@@ -1,10 +1,138 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, Subscription } from 'rxjs';
+
+import { ClienteService } from '../../../service/cliente.service';
+import { Cliente } from '../../../models/cliente.model';
+
+import { FormularioClienteComponent } from '../formulario-cliente/formulario-cliente.component';
+
+import Swal from 'sweetalert2';
+
+import { RoleId } from '../../../shared/types/Roles.types';
+import { Permission } from '../../../shared/types/permissions.types';
+import { JwtService } from '../../../service/jwt.service';
+
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-listado-cliente',
   templateUrl: './listado-cliente.component.html',
-  styleUrl: './listado-cliente.component.scss'
+  styleUrl: './listado-cliente.component.scss',
 })
-export class ListadoClienteComponent {
+export class ListadoClienteComponent implements OnInit, OnDestroy {
+  public clientes: Cliente[] = [];
+  refClient: Observable<any>;
+  subs: Subscription[] = [];
+  public isLoaded = false;
+  public dataSource: Cliente[] = [];
+  displayedColumns: string[] = [
+    'id',
+    'nombre',
+    'apellido',
+    'telefono',
+    'email',
+  ];
+  success = false;
+  roleIds = RoleId;
+  permissions = new Permission();
 
+  constructor(
+    private _clienteService: ClienteService,
+    private dialog: MatDialog,
+    private spinnerService: NgxSpinnerService,
+    private jwtService: JwtService,
+    private cdRef: ChangeDetectorRef
+  ) {
+    this.refClient = this._clienteService.getList();
+  }
+
+  ngOnInit(): void {
+    this.spinnerService.show();
+    this.handleClientRef();
+    this.addActionColumn();
+  }
+
+  ngOnDestroy(): void {
+    this._clienteService.list = [];
+    this.subs.map((sub) => sub.unsubscribe());
+  }
+
+  deleteClient(id: number): any {
+    Swal.fire({
+      title: '¿Esta seguro de eliminar este dato?',
+      text: '¡No se podra recuperar este dato luego de ser eliminado!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No',
+    }).then((result) => {
+      if (result.value) {
+        this.spinnerService.show();
+        this._clienteService.deleteClient(id).subscribe((data) => {
+          this.success = true;
+          Swal.fire('Eliminado!', 'El dato ha sido eliminado.', 'success');
+          this.spinnerService.hide();
+        });
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        (error: string) =>
+          console.log('Hubo un fallo al momento de eliminar el dato' + error);
+        Swal.fire(
+          'Cancelado',
+          'El dato no fue eliminado y esta seguro :)',
+          'error'
+        );
+      }
+    });
+  }
+
+  openDialog(tipo: string, id?: number): void {
+    if (tipo === 'c') {
+      this.dialog.open(FormularioClienteComponent, {
+        data: { type: tipo },
+      });
+    } else {
+      const client = this.clientes.find((d) => d.id === id);
+      this.dialog.open(FormularioClienteComponent, {
+        data: { type: tipo, client: client },
+      });
+    }
+  }
+
+  hasRole(roleId: number) {
+    return this.jwtService.hasRole(roleId);
+  }
+
+  hasPermission(permissionId: number) {
+    return this.jwtService.hasPermission(permissionId);
+  }
+
+  private handleClientRef(): void {
+    this.refClient.subscribe((data) => {
+      this.isLoaded = data !== null;
+      this.clientes = data || [];
+      this.dataSource = [...this.clientes];
+      this.cdRef.detectChanges();
+
+      if (this.isLoaded) {
+        this.spinnerService.hide();
+      }
+    });
+  }
+
+  hasUserPermissions(permissionIds: number[]) {
+    return this.jwtService.hasPermissions(permissionIds);
+  }
+
+  addActionColumn() {
+    const permissionIds = [
+      this.permissions.Cliente.Update,
+      this.permissions.Cliente.Delete,
+    ];
+
+    const hasPermission = this.hasUserPermissions(permissionIds);
+    if (hasPermission) {
+      this.displayedColumns.push('actions');
+    }
+  }
 }
