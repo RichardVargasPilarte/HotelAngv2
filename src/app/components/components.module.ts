2f4843ebbebc4fe7a85dfdb23c3bd841ffe5a567
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-06-27 18:55:41 -0600

    Refactor ComponentsModule to include Angular Material components and NgxSpinnerModule, and reorganize imports and exports.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { SharedModule } from "../shared/shared.module";
+
 import { ListadoAlojamientoComponent } from './alojamiento/listado-alojamiento/listado-alojamiento.component';
 import { FormularioAlojamientoComponent } from './alojamiento/formulario-alojamiento/formulario-alojamiento.component';
 import { ListadoHabitacionComponent } from './habitacion/listado-habitacion/listado-habitacion.component';
@@ -10,8 +14,39 @@ import { ActualizarUsuarioComponent } from './usuario/actualizar-usuario/actuali
 import { ListadoClienteComponent } from './cliente/listado-cliente/listado-cliente.component';
 import { FormularioClienteComponent } from './cliente/formulario-cliente/formulario-cliente.component';
 import { ListadoReservaComponent } from './reserva/listado-reserva/listado-reserva.component';
-import { FormularioReservaComponent } from './reserva/formulario-reserva/formulario-reserva.component';
 
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+
+const MaterialComponents = [
+  MatSidenavModule,
+  MatToolbarModule,
+  MatDividerModule,
+  MatButtonModule,
+  MatIconModule,
+  MatGridListModule,
+  MatTooltipModule,
+  MatTableModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule,
+  MatDatepickerModule
+];
 
 
 @NgModule({
@@ -25,11 +60,16 @@ import { FormularioReservaComponent } from './reserva/formulario-reserva/formula
     ActualizarUsuarioComponent,
     ListadoClienteComponent,
     FormularioClienteComponent,
-    ListadoReservaComponent,
-    FormularioReservaComponent
+    ListadoReservaComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedModule,
+    MaterialComponents,
+    NgxSpinnerModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [MaterialComponents, NgxSpinnerModule]
 })
 export class ComponentsModule { }
