1662c3e662be5dfc7892244ff23520a1e8971199
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-06-28 09:44:51 -0600

    Add comprehensive routing and user guard implementation in AppRoutingModule and UserGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,110 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './auth/login/login.component';
+import { CambiarContrasenaOlvidadaComponent } from './auth/cambiar-contrasena-olvidada/cambiar-contrasena-olvidada.component';
+
+import { ListadoAlojamientoComponent } from './components/alojamiento/listado-alojamiento/listado-alojamiento.component';
+import { ListadoHabitacionComponent } from './components/habitacion/listado-habitacion/listado-habitacion.component';
+import { ListadoUsuarioComponent } from './components/usuario/listado-usuario/listado-usuario.component';
+import { ListadoClienteComponent } from './components/cliente/listado-cliente/listado-cliente.component';
+import { ListadoReservaComponent } from './components/reserva/listado-reserva/listado-reserva.component';
+
+import { UserGuard } from './guards/user.guard';
+
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
+import { MenuComponent } from './shared/menu/menu.component';
+
+export interface IRouteData {
+  isPublic: boolean;
+}
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'app/Menu',
+    pathMatch: 'full',
+    data: <IRouteData>{
+      isPublic: true,
+    }
+  },
+  {
+    path: 'resetpassword/:token',
+    component: CambiarContrasenaOlvidadaComponent,
+    title: 'Cambiar Contraseña',
+    canActivate: [UserGuard],
+    data: <IRouteData>{
+      isPublic: true,
+    }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [UserGuard],
+    title: 'Login',
+    data: <IRouteData>{
+      isPublic: true,
+    }
+  },
+  {
+    path: 'app',
+    canActivate: [UserGuard],
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'Alojamientos/Listado',
+        component: ListadoAlojamientoComponent,
+        title: 'Listado de Alojamientos',
+        data: <IRouteData>{
+          isPublic: false,
+        }
+      },
+      {
+        path: 'Habitaciones/Listado',
+        component: ListadoHabitacionComponent,
+        title: 'Listado de Habitaciones',
+        data: <IRouteData>{
+          isPublic: false,
+        }
+      },
+      {
+        path: 'Usuarios/Listado',
+        component: ListadoUsuarioComponent,
+        title: 'Listado de Usuarios',
+        data: <IRouteData>{
+          isPublic: false,
+        }
+      },
+      {
+        path: 'Clientes/Listado',
+        component: ListadoClienteComponent,
+        title: 'Listado de Clientes',
+        data: <IRouteData>{
+          isPublic: false,
+        }
+      },
+      {
+        path: 'Reservas/Listado',
+        component: ListadoReservaComponent,
+        title: 'Listado de Reservas',
+        data: <IRouteData>{
+          isPublic: false,
+        }
+      },
+      {
+        path: 'Menu',
+        component: MenuComponent,
+        title: 'Menu',
+        data: <IRouteData>{
+          isPublic: false,
+        }
+      },
+    ]
+
+  },
+  { path: '**', component: ListadoAlojamientoComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/guards/user.guard.ts
+++ src/app/guards/user.guard.ts
@@ -1,5 +1,28 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { IRouteData } from '../app-routing.module';
+import { JwtService } from '../service/jwt.service';
 
-export const userGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+
+export class UserGuard  {
+  constructor(
+    private jwt: JwtService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.jwt.loggedIn) {
+      return true;
+    } else {
+      if ((route.data as IRouteData).isPublic) return true;
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
