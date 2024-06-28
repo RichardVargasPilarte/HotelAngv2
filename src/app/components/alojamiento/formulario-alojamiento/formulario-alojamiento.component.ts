353167ac1c55512e9dafc4afc927e32d3e5fd759
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-06-28 08:47:52 -0600

    Implement accommodation listing and form components with CRUD operations and permissions handling

--- src/app/components/alojamiento/formulario-alojamiento/formulario-alojamiento.component.ts
+++ src/app/components/alojamiento/formulario-alojamiento/formulario-alojamiento.component.ts
@@ -1,10 +1,118 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  Validators,
+  FormGroup,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { AlojamientoService } from '../../../service/alojamiento.service';
+import {
+  Alojamiento,
+  IAlojamientoResponse,
+} from '../../../models/alojamiento.model';
+
+interface DialogData {
+  type: string;
+  alojam?: Alojamiento | IAlojamientoResponse;
+}
 
 @Component({
   selector: 'app-formulario-alojamiento',
   templateUrl: './formulario-alojamiento.component.html',
-  styleUrl: './formulario-alojamiento.component.scss'
+  styleUrl: './formulario-alojamiento.component.scss',
 })
 export class FormularioAlojamientoComponent {
+  public alojamientos = new Alojamiento();
+  public edit!: boolean;
+  subs: Subscription[] = [];
+  public selected = '0';
+  public form!: FormGroup;
 
+  constructor(
+    private alojamientoServicio: AlojamientoService,
+    public dialogRef: MatDialogRef<FormularioAlojamientoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private fb: FormBuilder
+  ) {
+    this.createForm();
+  }
+
+  createForm(id?: string): void {
+    if (this.data.type === 'c') {
+      this.form = this.fb.group({
+        id: new FormControl(0),
+        nombre: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(25),
+        ]),
+        descripcion: new FormControl('', [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(150),
+        ]),
+        tiempo_estadia: new FormControl('', [
+          Validators.required,
+          Validators.minLength(1),
+        ]),
+        eliminado: new FormControl('NO'),
+      });
+    } else {
+      const alojamiento: IAlojamientoResponse = this.data
+        .alojam as IAlojamientoResponse;
+      this.form = this.fb.group({
+        id: alojamiento.id,
+        nombre: new FormControl(alojamiento.nombre, [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(25),
+        ]),
+        descripcion: new FormControl(alojamiento.descripcion, [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(150),
+        ]),
+        tiempo_estadia: new FormControl(alojamiento.tiempo_estadia, [
+          Validators.required,
+          Validators.minLength(1),
+        ]),
+        eliminado: new FormControl('NO'),
+      });
+    }
+  }
+
+  saveAccommodation(): void {
+    let alojam = new Alojamiento();
+    alojam = Object.assign(alojam, this.form.value);
+    this.subs.push(
+      this.alojamientoServicio.addAccommodations(alojam).subscribe({
+        next: () => {
+          this.dialogRef.close();
+        },
+        error: (error: unknown) => console.log('Hubo un error' + error),
+      })
+    );
+  }
+
+  updateAccommodation(): void {
+    let alojam = new Alojamiento();
+    alojam = Object.assign(alojam, this.form.value);
+    this.subs.push(
+      this.alojamientoServicio
+        .updateAccommodation(alojam.id!, alojam)
+        .subscribe({
+          next: () => {
+            this.dialogRef.close();
+            (error: unknown) => console.log('Hubo un error' + error);
+          },
+        })
+    );
+  }
+
+  get Form(): unknown {
+    return this.form.controls;
+  }
 }
--- src/app/components/alojamiento/listado-alojamiento/listado-alojamiento.component.ts
+++ src/app/components/alojamiento/listado-alojamiento/listado-alojamiento.component.ts
@@ -1,10 +1,133 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, Subscription } from 'rxjs';
+
+import { AlojamientoService } from '../../../service/alojamiento.service';
+import { Alojamiento } from '../../../models/alojamiento.model';
+
+import { FormularioAlojamientoComponent } from '../formulario-alojamiento/formulario-alojamiento.component';
+
+import Swal from 'sweetalert2';
+
+import { NgxSpinnerService } from 'ngx-spinner';
+
+import { RoleId } from '../../../shared/types/Roles.types';
+import { Permission } from '../../../shared/types/permissions.types';
+import { JwtService } from '../../../service/jwt.service';
 
 @Component({
   selector: 'app-listado-alojamiento',
   templateUrl: './listado-alojamiento.component.html',
-  styleUrl: './listado-alojamiento.component.scss'
+  styleUrl: './listado-alojamiento.component.scss',
 })
-export class ListadoAlojamientoComponent {
+export class ListadoAlojamientoComponent implements OnInit, OnDestroy {
+  public alojamientos: Alojamiento[] = [];
+  refAloj: Observable<any>;
+  subs: Subscription[] = [];
+  public isLoaded = false;
+  public dataSource: Alojamiento[] = [];
+  displayedColumns: string[] = [
+    'id',
+    'nombre',
+    'descripcion',
+    'tiempo_estadia',
+  ];
+  success = false;
+  roleIds = RoleId;
+  permissions = new Permission();
 
+  constructor(
+    private _alojamientoService: AlojamientoService,
+    private dialog: MatDialog,
+    private spinnerService: NgxSpinnerService,
+    private jwtService: JwtService
+  ) {
+    this.refAloj = this._alojamientoService.getList();
+  }
+
+  ngOnInit(): void {
+    this.spinnerService.show();
+    this.handleAlojRef();
+    this.addActionsColumn();
+  }
+
+  ngOnDestroy(): void {
+    this._alojamientoService.list = [];
+    this.subs.map((sub) => sub.unsubscribe());
+  }
+
+  removeAccommodation(id: number): any {
+    Swal.fire({
+      title: '¿Esta seguro de eliminar este dato?',
+      text: '¡No se podra recuperar este dato luego de ser eliminado!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No',
+    }).then((result) => {
+      if (result.value) {
+        this._alojamientoService.deleteAccommodation(id).subscribe((data) => {
+          this.success = true;
+          Swal.fire('Eliminado!', 'El dato ha sido eliminado.', 'success');
+        });
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        (error: string) =>
+          console.log('Hubo un fallo al momento de eliminar el dato' + error);
+        Swal.fire(
+          'Cancelado',
+          'El dato no fue eliminado y esta seguro :)',
+          'error'
+        );
+      }
+    });
+  }
+
+  openDialog(tipo: string, id?: number): void {
+    if (tipo === 'c') {
+      this.dialog.open(FormularioAlojamientoComponent, {
+        data: { type: tipo },
+      });
+    } else {
+      const alojam = this.alojamientos.find((d) => d.id === id);
+      this.dialog.open(FormularioAlojamientoComponent, {
+        data: { type: tipo, alojam },
+      });
+    }
+  }
+
+  hasRole(roleId: number) {
+    return this.jwtService.hasRole(roleId);
+  }
+
+  hasPermission(permissionId: number) {
+    return this.jwtService.hasPermission(permissionId);
+  }
+
+  private handleAlojRef(): void {
+    this.refAloj.subscribe((data) => {
+      this.isLoaded = data !== null;
+      this.alojamientos = data || [];
+      this.dataSource = [...this.alojamientos];
+
+      if (this.isLoaded) {
+        this.spinnerService.hide();
+      }
+    });
+  }
+
+  hasUserPermissions(permissionIds: number[]) {
+    return this.jwtService.hasPermissions(permissionIds);
+  }
+
+  addActionsColumn() {
+    const permissionsIds = [
+      this.permissions.Alojamiento.Update,
+      this.permissions.Alojamiento.Delete,
+    ];
+
+    const hasPermissions = this.jwtService.hasPermissions(permissionsIds);
+    if (hasPermissions) {
+      this.displayedColumns.push('actions');
+    }
+  }
 }
