aa07110d83ff53d1493d545c59c5d69ed74a25f2
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-06-30 08:28:36 -0600

    Add trailing slash to API_EndPoint in production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   API_Domain: 'hotel-api-v2.onrender.com',
   API_Port: null,
-  API_EndPoint: 'api',
+  API_EndPoint: 'api/',
   API_URI: 'https://hotel-api-v2.onrender.com/api',
   API_Auth: 'https://hotel-api-v2.onrender.com/api/token/',
   Api_Auth_Verify: 'https://hotel-api-v2.onrender.com/api/token/verify/',
