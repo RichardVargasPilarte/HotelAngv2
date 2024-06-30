bdcebf09893ae848934b1fd2355d5c123df5d97d
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-06-30 08:27:32 -0600

    Set `API_Port` to `null` in production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_Domain: 'hotel-api-v2.onrender.com',
-  API_Port: 80,
+  API_Port: null,
   API_EndPoint: 'api',
   API_URI: 'https://hotel-api-v2.onrender.com/api',
   API_Auth: 'https://hotel-api-v2.onrender.com/api/token/',
