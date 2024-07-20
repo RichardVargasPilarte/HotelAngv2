f72efd76bdcb9bba7067a2bd959b2635a4b3d746
RicardoPilarte <hotelometepe@gmail.com>
2024-07-19 21:43:51 -0600

    new domain django

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
 export const environment = {
   production: true,
-  API_Domain: 'djangorailway-production.up.railway.app',
+  API_Domain: 'hotelapi-production.up.railway.app',
   API_Port: null,
   API_EndPoint: 'api/',
-  API_URI: 'https://djangorailway-production.up.railway.app/api',
-  API_Auth: 'https://djangorailway-production.up.railway.app/api/token/',
-  Api_Auth_Verify: 'https://djangorailway-production.up.railway.app/api/token/verify/',
+  API_URI: 'https://hotelapi-production.up.railway.app/api',
+  API_Auth: 'https://hotelapi-production.up.railway.app/api/token/',
+  Api_Auth_Verify:
+    'https://hotelapi-production.up.railway.app/api/token/verify/',
   APP_name: 'SPA',
 };
